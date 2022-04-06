3615a7fd60ea0ec6ee3f93158a8d6b4f02d23560
SHIVAM PANDEY <shivampandeyyofficial@gmail.com>
2022-04-07 05:25:28 +0530

    Modified

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -4,11 +4,11 @@ import PizzaCard from "./PizzaCard";
 const PizzaList = ({ pizzaList }) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Pizzas with 2X Toppings</h1>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
       <p className={styles.desc}>
-        ver looked at Pizza Toppings and wondered, "WHY SO LESS"?
-        Well, we did. And we definitely wanted more! So, we immediately took it upon ourselves to fix it, and made the pizzas that we all deserve - PIZZAS LOADED WITH 2X TOPPINGS.
-        And that's how Pizzahunt was born, to make sure that you Never Settle For Less.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
+        in pretium molestie. Interdum et malesuada fames acme. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit.
       </p>
       <div className={styles.wrapper}>
         {pizzaList.map((pizza) => (
