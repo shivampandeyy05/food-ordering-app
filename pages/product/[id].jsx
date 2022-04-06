4ef5ce036ba7c657108f9f6ac510b24bfe10bcc4
SHIVAM PANDEY <shivampandeyyofficial@gmail.com>
2022-04-07 05:09:22 +0530

    Modified

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -97,7 +97,7 @@ const Product = ({ pizza }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/products/${params.id}`
+    `https://pizzaaahunt.herokuapp.com/api/products/${params.id}`
   );
   return {
     props: {
