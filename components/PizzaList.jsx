e57977d4677ae7508efb3e2496b5b8fd83918e2a
SHIVAM PANDEY <shivampandeyyofficial@gmail.com>
2022-04-07 10:06:31 +0530

    Modified

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -6,8 +6,8 @@ const PizzaList = ({ pizzaList }) => {
     <div className={styles.container}>
       <h1 className={styles.title}>PIZZAS WITH LOTS OF TOPPINGS</h1>
       <p className={styles.desc}>
-        Ever looked at Pizza Toppings and wondered, "WHY SO LESS"?
-        Well, we did. And we definitely wanted more! So, we immediately took it upon ourselves to fix it, and made the pizzas that we all deserve - PIZZAS LOADED WITH 2X TOPPINGS.
+        {/* Ever looked at Pizza Toppings and wondered, "WHY SO LESS"?
+        Well, we did. And we definitely wanted more! So, we immediately took it upon ourselves to fix it, and made the pizzas that we all deserve - PIZZAS LOADED WITH 2X TOPPINGS. */}
       </p>
       <div className={styles.wrapper}>
         {pizzaList.map((pizza) => (
