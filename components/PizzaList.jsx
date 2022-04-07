4c1c3094108d9e54234d90359c66260bba864fb7
SHIVAM PANDEY <shivampandeyyofficial@gmail.com>
2022-04-07 10:09:42 +0530

    Modified

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -4,10 +4,10 @@ import PizzaCard from "./PizzaCard";
 const PizzaList = ({ pizzaList }) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>PIZZAS WITH LOTS OF TOPPINGS</h1>
+      <h1 className={styles.title}>PIZZAS WITH 2X TOPPINGS</h1>
       <p className={styles.desc}>
-        {/* Ever looked at Pizza Toppings and wondered, "WHY SO LESS"?
-        Well, we did. And we definitely wanted more! So, we immediately took it upon ourselves to fix it, and made the pizzas that we all deserve - PIZZAS LOADED WITH 2X TOPPINGS. */}
+        Ever looked at Pizza Toppings and wondered, "WHY SO LESS"?
+        Well, we did. And we definitely wanted more! So, we immediately took it upon ourselves to fix it, and made the pizzas that we all deserve.
       </p>
       <div className={styles.wrapper}>
         {pizzaList.map((pizza) => (
